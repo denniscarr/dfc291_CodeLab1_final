bdf2e11c5da3ce9904a9a9dcb2e2b1cf6039df66
denniscarr <dcarr1@gmail.com>
2017-04-19 20:28:30 -0400

    Refactored 'MainMenuUI' (Previously named MainMenuScript)

--- Static/Assets/Scripts/MainMenuUI.cs
+++ Static/Assets/Scripts/MainMenuUI.cs
@@ -3,25 +3,33 @@ using UnityEngine.SceneManagement;
 using UnityStandardAssets.Characters.FirstPerson;
 using System.Collections;
 
-public class MainMenuScript : MonoBehaviour {
+public class MainMenuUI : MonoBehaviour {
 
-	public void PlayButton() {
-        // Unpause everything and hide menu.
+	public void PlayButton()
+    {
+        // Unpause enemies in the background.
         foreach (GameObject enemy in GameObject.FindGameObjectsWithTag("Enemy"))
         {
             enemy.GetComponent<Enemy>().enabled = true;
         }
+
+        // Enable player movement and shooting.
         GameObject.Find("FPSController").GetComponent<FirstPersonController>().enabled = true;
         GameObject.Find("Gun").GetComponent<GunScript>().enabled = true;
 
+        // Turn off the main menu.
         transform.parent.gameObject.SetActive(false);
     }
 
-	public void QuitButton() {
+
+	public void QuitButton()
+    {
 		Application.Quit ();
 	}
 
-	public void InfoButton() {
-		SceneManager.LoadScene (1);
+
+	public void InfoButton()
+    {
+		SceneManager.LoadScene ("InfoScene");
 	}
 }
